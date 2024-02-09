f1fd33be6b2340dfa5b7fb8a9cc318ef5ffe30a4
demo-23home <zeyadslama23@gmail.com>
2024-02-09 22:01:14 +0200

    Adding Swagger-UI

--- djChat/djChat/settings.py
+++ djChat/djChat/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     # Internal Apps:
     "account",
     "server",
+    # External Modules
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -94,16 +96,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": 
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": 
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": 
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": 
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -132,3 +138,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = "account.Account"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}
