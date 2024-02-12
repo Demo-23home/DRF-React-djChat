7611a9482114d58d7ec80c3fff64b3fcd640c743
demo-23home <zeyadslama23@gmail.com>
2024-02-12 21:32:21 +0200

    Configuring Static & Media settings

--- djChat/djChat/settings.py
+++ djChat/djChat/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
+MEDIA_URL = "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -143,7 +145,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),  # Ensure the comma is present here
 }
-
 
 
 SPECTACULAR_SETTINGS = {
--- djChat/djChat/urls.py
+++ djChat/djChat/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import (
@@ -24,7 +26,6 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
-
 
 router = DefaultRouter()
 router.register("api/server/select", ServerListViewSet)
@@ -42,6 +43,10 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-
     # path("api/", include('server.urls'))
 ] + router.urls
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
