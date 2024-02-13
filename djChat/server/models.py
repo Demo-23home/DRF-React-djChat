19f8f15c1d52534feceeb708802296560c3a740a
demo-23home <zeyadslama23@gmail.com>
2024-02-13 02:28:31 +0200

    Adding Icon Field && Setting Icon To Be Deleted Whenever Category Is

--- djChat/server/models.py
+++ djChat/server/models.py
@@ -1,12 +1,34 @@
 from django.conf import settings
 from django.db import models
-
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
 # Create your models here.
+
+
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
+
 
     def __str__(self):
         return self.name
@@ -15,8 +37,7 @@ class Category(models.Model):
 class Server(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, 
-        related_name="server_owner"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
     )
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="server_category"
@@ -31,8 +52,7 @@ class Server(models.Model):
 class Channel(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, 
-        related_name="channel_owner"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
     )
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(
