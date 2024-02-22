34b4f6c12bd019c61dd2a686658ee3c6dd7a8f33
demo-23home <zeyadslama23@gmail.com>
2024-02-22 10:23:48 +0200

    Adding Responsive Font-sizes

--- reactChat/src/theme/theme.tsx
+++ reactChat/src/theme/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 
 declare module "@mui/material/styles" {
   interface Theme {
@@ -32,6 +32,7 @@ const createMuiTheme = () => {
       },
     },
   });
+  theme = responsiveFontSizes(theme);
   return theme;
 };
 
