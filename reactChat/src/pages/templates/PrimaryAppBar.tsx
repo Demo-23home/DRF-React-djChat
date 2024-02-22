194971712d2d28d379fc23282763bbb4122d06c2
demo-23home <zeyadslama23@gmail.com>
2024-02-22 12:48:37 +0200

    Adding Responsive AppDrawer

--- reactChat/src/pages/templates/PrimaryAppBar.tsx
+++ reactChat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,14 +1,40 @@
-import { AppBar, Link, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Link,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu"; // Corrected the import name
+import { useEffect, useState } from "react";
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.MouseEvent) => {
+    setSideMenu(open); // Set the side menu state to the provided value
+  };
+
   const theme = useTheme();
   const appBarHeight = theme.primaryAppBar.height || 0;
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
+
+  useEffect(() => {
+    if (isSmallScreen && sideMenu) {
+      setSideMenu(false);
+    }
+  },[isSmallScreen]);
 
   return (
     <>
       <AppBar
         sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 2,
           backgroundColor: theme.palette.background.default,
           borderBottom: `1px solid ${theme.palette.divider}`,
         }}
@@ -17,12 +43,32 @@ const PrimaryAppBar = () => {
           variant="dense"
           sx={{ height: appBarHeight, minHeight: appBarHeight }}
         >
+          <Box sx={{ display: { xs: "block", sm: "none" } }}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer(!sideMenu)} // Invert the sideMenu state
+            >
+              <MenuIcon /> {/* Corrected the component name */}
+            </IconButton>
+          </Box>
+
+          <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+            {[...Array(100)].map((_, i) => (
+              <Typography key={i} paragraph>
+                {i + 1}
+              </Typography>
+            ))}
+          </Drawer>
+
           <Link href="/" underline="none" color="inherit">
             <Typography
               variant="h6"
               noWrap
               component="div"
-              sx={{ display: { fontWeigt: 700, letterSpacing: "-0.5px" } }}
+              sx={{ fontWeight: 700, letterSpacing: "-0.5px" }} // Corrected the property name
             >
               DJCHAT
             </Typography>
